d2196001ff9f8ed5bcb8469c8be878c796332955
Jasper Martin <jasper.martin@readytech.io>
2025-03-06 17:49:06 +1100

    Don't add newline when saving.

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Prevent adding newline at end of file
+vim.opt.fixeol = false
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
