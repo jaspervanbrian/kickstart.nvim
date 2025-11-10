fe12825908039a8f1fd8b30cedfb5a7135fa3ca5
Jasper Martin <jasper.martin@readytech.io>
2025-11-10 15:50:38 +1100

    Use prettier on javascript and typescript files.

--- init.lua
+++ init.lua
@@ -924,6 +924,11 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
